fbed72447dd0dd858e10680fb5b97ff7ea540b08
Michael DeSisto <158067883+mdesisto02@users.noreply.github.com>
2024-04-19 16:54:56 -0400

    Update GEN_711_811_FinalProj.sh

--- GEN_711_811_FinalProj.sh
+++ GEN_711_811_FinalProj.sh
@@ -1,4 +1,31 @@
 #! /bin/bash
 
+mdkir ~/gen_proj
+
+cd ~/gen_proj
+
+cp -R /tmp/gen711_project_data/eDNA-fqs/cyano .
+
 conda activate qiime2-2022.8
 
+qiime tools import \
+   --type "SampleData[PairedEndSequencesWithQuality]"  \
+   --input-format CasavaOneEightSingleLanePerSampleDirFmt \
+   --input-path ~/gen_proj/cyano/fastqs \
+   --output-path ~/gen_proj/cyano_raw_reads.qza
+
+mkdir cutadapt_output
+
+qiime cutadapt trim-paired \
+    --i-demultiplexed-sequences ~/gen_proj/cyano_raw_reads.qza \
+    --p-cores 4 \
+    --p-front-f GTGYCAGCMGCCGCGGTAA \
+    --p-front-r CCGYCAATTYMTTTRAGTTT \
+    --p-discard-untrimmed \
+    --p-match-adapter-wildcards \
+    --verbose \
+    --o-trimmed-sequences cutadapt_output/raw_reads_no_primers.qza
+
+qiime demux summarize \
+--i-data <path to the file from step above> \
+--o-visualization  <path to an output directory>/<a name for the output files>.qzv 
